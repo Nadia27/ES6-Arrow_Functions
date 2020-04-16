6bd792669b842e41049560d51cb929395d6992bd
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-16 11:44:01 -0400

    update map function
    Update  emojipedia map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,16 +9,14 @@ function App() {
         <span>emojipedia</span>
       </h1>
       <dl className="dictionary">
-        {emojipedia.map(emojiTerm => {
-          return (
-            <Entry
-              key={emojiTerm.id}
-              emoji={emojiTerm.emoji}
-              name={emojiTerm.name}
-              description={emojiTerm.meaning}
-            />
-          );
-        })}
+        {emojipedia.map(emojiTerm => (
+          <Entry
+            key={emojiTerm.id}
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            description={emojiTerm.meaning}
+          />
+        ))}
       </dl>
     </div>
   );
